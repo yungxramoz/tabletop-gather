e8522f2df084906d76281218517e2a8bcd75db82
Simon Schödler <simon.schoedler@hotmail.ch>
2023-11-19 22:13:14 +0100

    Adds landing page

--- apps/frontend/src/app/components/pages/login.component.ts
+++ apps/frontend/src/app/components/pages/login.component.ts
@@ -2,6 +2,7 @@ import { AsyncPipe, CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { NbTabsetModule } from '@nebular/theme';
+import { ROUTE_EVENTS } from '../../constants';
 import { LoginUserDto } from '../../models/login-user.dto';
 import { Model } from '../../models/model.type';
 import { RegisterUserDto } from '../../models/register-user.dto';
@@ -20,18 +21,27 @@ import { RegisterFormComponent } from '../molecules/register-form.component';
     RegisterFormComponent,
   ],
   template: `
-    <nb-tabset fullWidth class="tg-max-w-50">
-      <nb-tab tabTitle="Login">
-        <tg-login-form
-          (credentialsCreated)="onCredentialsCreated($event)"
-        ></tg-login-form>
-      </nb-tab>
-      <nb-tab tabTitle="Register">
-        <tg-register-form
-          (userCreated)="onUserCreated($event)"
-        ></tg-register-form>
-      </nb-tab>
-    </nb-tabset>
+    <div class="tg-flex-col tg-justify-around">
+      <img
+        title="Tabletop Gather Logo"
+        src="assets/tg-wizard-no-bg.svg"
+        width="120"
+        height="120"
+        class="tg-m-4"
+      />
+      <nb-tabset fullWidth class="tg-max-w-50 tg-full">
+        <nb-tab tabTitle="Login">
+          <tg-login-form
+            (credentialsCreated)="onCredentialsCreated($event)"
+          ></tg-login-form>
+        </nb-tab>
+        <nb-tab tabTitle="Register">
+          <tg-register-form
+            (userCreated)="onUserCreated($event)"
+          ></tg-register-form>
+        </nb-tab>
+      </nb-tabset>
+    </div>
   `,
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -45,7 +55,7 @@ export class LoginComponent {
   public onCredentialsCreated(event: Model<LoginUserDto>) {
     // Unsubscribe, since this uses shareReplay()
     const subscription = this.authService.login(event).subscribe(() => {
-      this.router.navigate(['/']);
+      this.router.navigate(['/' + ROUTE_EVENTS]);
       subscription.unsubscribe();
     });
   }
