3254d3892be907d347c9fa089a15eeeb44a7ee90
Simon Schödler <simon.schoedler@hotmail.ch>
2023-11-20 22:13:50 +0100

    Improves avatar

--- apps/frontend/src/app/components/atoms/avatar.component.ts
+++ apps/frontend/src/app/components/atoms/avatar.component.ts
@@ -19,16 +19,16 @@ import { UserDto } from '../../models/user.dto';
         background-color: transparent;
         border-radius: 50%;
         border: 1px solid var(--color-control-default);
-        box-shadow: 0 0 5rem var(--border-basic-color-4);
-        height: 10rem;
+        box-shadow: 0 0 3rem var(--border-basic-color-4);
+        height: 8rem;
         text-align: center;
-        width: 10rem;
+        width: 8rem;
       }
 
       .initials {
-        line-height: 5;
+        line-height: 4;
         position: relative;
-        top: calc(10rem / 4); /* 25% of parent */
+        top: calc(8rem / 4); /* 25% of parent */
       }
     `,
   ],
