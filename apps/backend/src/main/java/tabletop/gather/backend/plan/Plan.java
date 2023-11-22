7da09631f19c927dda3492d212347918ab89d4be
Sandro Gerber <sandro-gerber@outlook.com>
2023-11-22 22:05:03 +0100

    Adds create plan endpoint

--- apps/backend/src/main/java/tabletop/gather/backend/plan/Plan.java
+++ apps/backend/src/main/java/tabletop/gather/backend/plan/Plan.java
@@ -24,45 +24,45 @@ import java.util.UUID;
 @Setter
 public class Plan {
 
-    @Id
-    @Column(nullable = false, updatable = false)
-    @GenericGenerator(name = "uuid", strategy = "org.hibernate.id.UUIDGenerator")
-    @GeneratedValue(generator = "uuid")
-    private UUID id;
+  @Id
+  @Column(nullable = false, updatable = false)
+  @GenericGenerator(name = "uuid", strategy = "org.hibernate.id.UUIDGenerator")
+  @GeneratedValue(generator = "uuid")
+  private UUID id;
 
 
-    @Column(nullable = false)
-    private String name;
+  @Column(nullable = false)
+  private String name;
 
-    @Column(nullable = false)
-    private Boolean isPrivate;
+  @Column(nullable = false)
+  private Boolean isPrivate;
 
-    @Column(columnDefinition = "TEXT", nullable = false)
-    private String description;
+  @Column(columnDefinition = "TEXT", nullable = false)
+  private String description;
 
-    @Column()
-    private int playerLimit;
+  @Column()
+  private int playerLimit;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id")
+  private User user;
 
-    @OneToMany(mappedBy = "plan")
-    private Set<Gathering> gatherings;
+  @OneToMany(mappedBy = "plan", cascade = CascadeType.ALL, orphanRemoval = true)
+  private Set<Gathering> gatherings;
 
-    @OneToMany(mappedBy = "plan")
-    private Set<Comment> comments;
+  @OneToMany(mappedBy = "plan")
+  private Set<Comment> comments;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "game_id")
-    private Game game;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "game_id")
+  private Game game;
 
-    @CreatedDate
-    @Column(nullable = false, updatable = false)
-    private OffsetDateTime dateCreated;
+  @CreatedDate
+  @Column(nullable = false, updatable = false)
+  private OffsetDateTime dateCreated;
 
-    @LastModifiedDate
-    @Column(nullable = false)
-    private OffsetDateTime lastUpdated;
+  @LastModifiedDate
+  @Column(nullable = false)
+  private OffsetDateTime lastUpdated;
 
 }
