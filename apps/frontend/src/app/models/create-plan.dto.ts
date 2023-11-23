e12948e67285efb42fad54d5dc7026dee9dc3100
Simon Schödler <simon.schoedler@hotmail.ch>
2023-11-23 16:45:55 +0100

    Updates plan models

--- apps/frontend/src/app/models/create-plan.dto.ts
+++ apps/frontend/src/app/models/create-plan.dto.ts
@@ -1,18 +1,25 @@
 import { Model } from '../utils/types';
-import { Gathering } from './gathering.dto';
-import { PlanDto } from './plan.dto';
+import { CreateGathering } from './create-gathering.dto';
+import { Dto } from './dto.base';
+import { GameDto } from './game.dto';
 
 /**
  * Dto for creating plans.
  *
- * @property {{ date: Gathering['date'], startTime: Gathering['startTime'] }[]} gatherings - The gatherings of the plan
- * @extends {PlanDto} - {@see {@link PlanDto}}
+ * @property {string} name - The name of the plan
+ * @property {string} description - The description of the plan
+ * @property {boolean} isPrivate - Whether the plan is private or not
+ * @property {number} playerLimit - The player limit of the plan
+ * @property {GameDto[id]} gameId - The game of the plan
+ * @property {CreateGathering[]} gatherings - The gatherings of the plan
  */
-export type CreatePlanDto = Omit<PlanDto, 'gatherings'> & {
-  gatherings: {
-    date: Gathering['date'];
-    startTime: Gathering['startTime'];
-  }[];
-};
+export class CreatePlanDto extends Dto {
+  public name!: string;
+  public isPrivate!: boolean;
+  public description!: string;
+  public playerLimit!: number;
+  public gameId!: GameDto['id'];
+  public gatherings: CreateGathering[] = [];
+}
 
 export type CreatePlan = Model<CreatePlanDto>;
--- apps/frontend/src/app/models/detail-plan.dto.ts
+++ apps/frontend/src/app/models/detail-plan.dto.ts
@@ -1,25 +1,25 @@
-import { Model } from '../utils/types';
+import { DetailGatheringDto } from './detail-gathering.dto';
 import { Dto } from './dto.base';
 import { GameDto } from './game.dto';
 import { UserDto } from './user.dto';
 
 /**
- * Dto for creating plans.
+ * Dto for plan details.
  *
  * @property {string} name - The name of the plan
  * @property {string} description - The description of the plan
  * @property {boolean} isPrivate - Whether the plan is private or not
  * @property {number} playerLimit - The player limit of the plan
- * @property {User['id']} user - The user of the plan
- * @property {GameDto['id']} game - The game of the plan
+ * @property {UserDto} owner - The owner of the plan
+ * @property {GameDto} game - The game of the plan
+ * @property {DetailGatheringDto[]} gatherings - The gatherings of the plan
  */
-export class PlanDto extends Dto {
+export class DetailPlanDto extends Dto {
   public name!: string;
   public isPrivate!: boolean;
   public description!: string;
   public playerLimit!: number;
-  public user!: UserDto['id'];
-  public game!: GameDto['id'];
+  public owner!: UserDto;
+  public game!: GameDto;
+  public gatherings: DetailGatheringDto[] = [];
 }
-
-export type Plan = Model<PlanDto>;
