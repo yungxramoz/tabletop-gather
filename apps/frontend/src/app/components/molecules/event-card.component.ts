b3de81a69a43d3f7e5614e3831acaf2c08a438c6
Simon Schödler <simon.schoedler@hotmail.ch>
2023-11-24 12:27:43 +0100

    Adds event cards for event component

---
+++ apps/frontend/src/app/components/molecules/event-card.component.ts
@@ -0,0 +1,59 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
+import { NbButtonModule, NbCardModule, NbIconModule } from '@nebular/theme';
+import { OverviewPlanDto } from '../../models/overview-plan.dto';
+
+@Component({
+  standalone: true,
+  selector: 'tg-event-card',
+  imports: [NbCardModule, NbButtonModule, NbIconModule],
+  template: `
+    <nb-card>
+      <nb-card-header>
+        <div class="tg-block">
+          <p class="caption-2">
+            {{ overviewPlanDto.gatheringDates.join(', ') }}
+          </p>
+        </div>
+        {{ overviewPlanDto.name }}
+      </nb-card-header>
+      <nb-card-body>
+        {{ overviewPlanDto.description }}
+      </nb-card-body>
+      <nb-card-footer>
+        <div class="tg-flex-row tg-justify-end">
+          <button
+            nbButton
+            ghost
+            status="danger"
+            shape="semi-round"
+            (click)="delete.emit()"
+          >
+            <nb-icon icon="trash-2-outline"></nb-icon>
+          </button>
+          <div class="tg-m-1"></div>
+          <button
+            nbButton
+            ghost
+            status="control"
+            shape="semi-round"
+            (click)="edit.emit()"
+          >
+            Edit
+          </button>
+        </div>
+      </nb-card-footer>
+    </nb-card>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class EventCardComponent {
+  @Input({ required: true }) public overviewPlanDto!: OverviewPlanDto;
+  @Output() public delete: EventEmitter<void> = new EventEmitter();
+  @Output() public edit: EventEmitter<void> = new EventEmitter();
+}
--- apps/frontend/src/app/components/pages/events.component.ts
+++ apps/frontend/src/app/components/pages/events.component.ts
@@ -1,12 +1,32 @@
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NbButtonModule, NbCardModule, NbIconModule } from '@nebular/theme';
-import { ROUTE_PLAN_EVENT } from '../../constants';
+import { Router, RouterModule } from '@angular/router';
+import {
+  NbButtonModule,
+  NbCardModule,
+  NbIconModule,
+  NbListModule,
+} from '@nebular/theme';
+import { ROUTE_MANAGE_EVENT, ROUTE_PLAN_EVENT } from '../../constants';
+import { PlanService } from '../../services/plan.service';
+import { VoidComponent } from '../atoms/void.component';
+import { EventCardComponent } from '../molecules/event-card.component';
 
 @Component({
   standalone: true,
   selector: 'tg-events',
-  imports: [NbCardModule, NbButtonModule, NbIconModule, RouterModule],
+  imports: [
+    NgIf,
+    NgFor,
+    AsyncPipe,
+    NbCardModule,
+    NbListModule,
+    NbButtonModule,
+    NbIconModule,
+    RouterModule,
+    EventCardComponent,
+    VoidComponent,
+  ],
   template: `
     <nb-card>
       <button
@@ -20,9 +40,40 @@ import { ROUTE_PLAN_EVENT } from '../../constants';
         Create Event
       </button>
     </nb-card>
+
+    <ng-container *ngIf="myPlans$ | async as myPlans; else noPrivatePlans">
+      <ng-container *ngIf="myPlans.length > 0; else noPrivatePlans">
+        <tg-event-card
+          *ngFor="let plan of myPlans"
+          [overviewPlanDto]="plan"
+          (edit)="editMyEvent(plan.id)"
+          (delete)="deleteMyEvent(plan.id)"
+        >
+        </tg-event-card>
+      </ng-container>
+    </ng-container>
+    <ng-template #noPrivatePlans>
+      <nb-card-body>
+        <tg-void message="You have no private events"></tg-void>
+      </nb-card-body>
+    </ng-template>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EventsComponent {
   public readonly routePlanEvent = '/' + ROUTE_PLAN_EVENT;
+  public myPlans$ = this.planService.getAllMyPlans();
+
+  public constructor(
+    private readonly planService: PlanService,
+    private readonly router: Router
+  ) {}
+
+  public editMyEvent(planId: string) {
+    this.router.navigate(['/' + ROUTE_MANAGE_EVENT, planId]);
+  }
+
+  public deleteMyEvent(planId: string) {
+    console.log('delete', planId);
+  }
 }
