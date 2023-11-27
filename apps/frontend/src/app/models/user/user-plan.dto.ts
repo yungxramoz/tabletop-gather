a2119a88cfc78fb57acecbc4d51f79c6f756f643
Simon Schödler <simon.schoedler@hotmail.ch>
2023-11-28 00:06:52 +0100

    Adds new Dtos

---
+++ apps/frontend/src/app/models/user/user-plan.dto.ts
@@ -0,0 +1,17 @@
+import { Model } from '../../utils/types';
+import { Dto } from '../base.dto';
+import { DateTimeGatheringDto } from '../gathering/date-time-gathering.dto';
+import { UserDto } from './user.dto';
+
+/**
+ * Dto for a users which attends an event (plan).
+ *
+ * @property {`${UserDto['firstName']} ${UserDto['lastName']}`} fullName - The full name of the user
+ * @property {DateTimeGatheringDto[]} attendingGatherings - The gatherings the user could attend
+ */
+export class UserPlanDto extends Dto {
+  public fullName!: `${UserDto['firstName']} ${UserDto['lastName']}`;
+  public attendingGatherings!: DateTimeGatheringDto[];
+}
+
+export type UserPlan = Model<UserPlanDto>;
