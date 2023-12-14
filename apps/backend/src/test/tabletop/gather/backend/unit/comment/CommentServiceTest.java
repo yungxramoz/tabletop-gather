544c5b8c500b52a5fa67cba7fe1a0ef985295aa4
Sandro Gerber <sandro-gerber@outlook.com>
2023-12-14 19:40:11 +0100

    Fixes lint error

--- apps/backend/src/test/tabletop/gather/backend/unit/comment/CommentServiceTest.java
+++ apps/backend/src/test/tabletop/gather/backend/unit/comment/CommentServiceTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 import java.util.*;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
