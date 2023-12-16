04193f8bd7b7dfe35de98a22bc8c766d96c74b2d
Simon Schödler <simon.schoedler@hotmail.ch>
2023-12-16 15:36:45 +0100

    Adds id to commentitem

--- apps/backend/src/main/java/tabletop/gather/backend/comment/CommentItemDto.java
+++ apps/backend/src/main/java/tabletop/gather/backend/comment/CommentItemDto.java
@@ -1,12 +1,15 @@
 package tabletop.gather.backend.comment;
 
 import java.time.OffsetDateTime;
+import java.util.UUID;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class CommentItemDto {
+
+  private UUID id;
 
   private String comment;
 
