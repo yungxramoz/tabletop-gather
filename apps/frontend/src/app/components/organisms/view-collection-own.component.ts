f107a9ab86897be110ae6a9c4d1f5ebbb6fa43cb
Simon Schödler <simon.schoedler@hotmail.ch>
2023-12-16 15:39:30 +0100

    Fixes wrong configured delete dialogues

--- apps/frontend/src/app/components/organisms/view-collection-own.component.ts
+++ apps/frontend/src/app/components/organisms/view-collection-own.component.ts
@@ -1,10 +1,19 @@
 import { AsyncPipe, NgFor } from '@angular/common';
-import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
+import { NbDialogService } from '@nebular/theme';
+import { Observable, filter } from 'rxjs';
+import { Game, GameDto } from '../../models/game/game.dto';
 import { GameCardComponent } from '../molecules/game-card.component';
-import {filter, Observable} from "rxjs";
-import {Game, GameDto} from "../../models/game/game.dto";
-import {NbDialogService} from "@nebular/theme";
-import {DeleteDialogComponent, DeleteDialogResult} from "./delete-dialog.component";
+import {
+  DeleteDialogComponent,
+  DeleteDialogResult,
+} from './delete-dialog.component';
 
 @Component({
   standalone: true,
@@ -14,7 +23,7 @@ import {DeleteDialogComponent, DeleteDialogResult} from "./delete-dialog.compone
     <ng-container *ngFor="let game of games | async">
       <tg-game-card
         [game]="game"
-        [actionButton]="{icon: 'trash-2-outline', status: 'danger'}"
+        [actionButton]="{ icon: 'trash-2-outline', status: 'danger' }"
         (actionButtonClicked)="handleRemoveFromCollection($event)"
       ></tg-game-card>
     </ng-container>
@@ -25,18 +34,19 @@ export class ViewCollectionOwnComponent {
   @Input({ required: true }) public games!: Observable<Game[]>;
   @Output() public deleteFromCollection = new EventEmitter<GameDto>();
 
-  public constructor(
-    private readonly dialogService: NbDialogService,
-  ) {}
+  public constructor(private readonly dialogService: NbDialogService) {}
 
   public handleRemoveFromCollection(game: GameDto) {
     this.dialogService
       .open(DeleteDialogComponent, {
-        context: 'Do you really want to remove this game from your collection?',
+        context: {
+          message:
+            'Do you really want to remove this game from your collection?',
+        },
       })
       .onClose.pipe(
         filter((result: DeleteDialogResult) => result !== undefined),
-        filter((result) => result.delete),
+        filter((result) => result.delete)
       )
       .subscribe(() => {
         this.deleteFromCollection.emit(game);
